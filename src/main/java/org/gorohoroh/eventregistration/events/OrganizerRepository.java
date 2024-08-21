ce80942af63fac2c2407c418e3b336ead6c7e39b
Jura Gorohovsky <gorohoroh@gmail.com>
2024-08-21 20:00:58 +0600

    Repositories for Event, Organizer, Product, Venue, Registration

---
+++ src/main/java/org/gorohoroh/eventregistration/events/OrganizerRepository.java
@@ -0,0 +1,16 @@
+package org.gorohoroh.eventregistration.events;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class OrganizerRepository {
+
+    private final List<Organizer> organizers = List.of(
+            new Organizer(101, "Globomantics", "Globomantics Technology Corporation"),
+            new Organizer(102, "Carved Rock", "Carved Rock Sports Equipment")
+    );
+
+    public List<Organizer> findAll() { return organizers; }
+}
