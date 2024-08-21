b67d92543397f303487558d53ad89dd8a9f4f7d5
Jura Gorohovsky <gorohoroh@gmail.com>
2024-08-22 00:06:02 +0600

    Data validation

--- src/main/java/org/gorohoroh/eventregistration/registrations/Registration.java
+++ src/main/java/org/gorohoroh/eventregistration/registrations/Registration.java
@@ -1,4 +1,11 @@
 package org.gorohoroh.eventregistration.registrations;
 
-public record Registration(int id, int productId, String ticketCode, String attendeeName) {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+
+public record Registration(
+        Integer id,
+        @NotNull(message = "Product ID is required") Integer productId,
+        String ticketCode,
+        @NotBlank(message = "Attendee name is required") String attendeeName) {
 }
