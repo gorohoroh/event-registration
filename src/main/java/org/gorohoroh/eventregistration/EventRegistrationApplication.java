95c46199a582537707cce889c7fac4e1a85e1b96
Jura Gorohovsky <gorohoroh@gmail.com>
2024-08-21 18:43:52 +0600

    Reformatting

--- src/main/java/org/gorohoroh/eventregistration/EventRegistrationApplication.java
+++ src/main/java/org/gorohoroh/eventregistration/EventRegistrationApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class EventRegistrationApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventRegistrationApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(EventRegistrationApplication.class, args);
+    }
 
 }
