c4b488c77364cfee1c1280365f8401fe0f6c9286
Jura Gorohovsky <gorohoroh@gmail.com>
2024-08-22 13:40:21 +0600

    Storing eveng information in Postgres

--- src/main/java/org/gorohoroh/eventregistration/events/Organizer.java
+++ src/main/java/org/gorohoroh/eventregistration/events/Organizer.java
@@ -1,4 +1,45 @@
 package org.gorohoroh.eventregistration.events;
 
-public record Organizer(int id, String name, String description) {
+import jakarta.persistence.*;
+
+import java.util.Objects;
+
+@Entity
+@Table(name = "organizers")
+public class Organizer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column
+    private String description;
+
+    public int getId() {
+        return id;
+    }
+    
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
 }
